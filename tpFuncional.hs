7276b36c8d6225214b3d88fff724cf56ac7d15b1
Soledad Leiva <soledadleiva@MacBook-de-Soledad.local>
2019-05-05 22:24:01 -0300

    correccion

--- tpFuncional.hs
+++ tpFuncional.hs
@@ -1,31 +1,47 @@
+import Text.Show.Functions
+
 data Auto = Auto {
-    nombre :: String,
-    nivelNafta :: Float,
-    velocidad :: Float,
-    nombreEnamorade :: String,
-    trucoParticular :: String
+    nombre :: Nombre,
+    nivelNafta :: NivelNafta,
+    velocidad :: Velocidad,
+    nombreEnamorade :: Nombre,
+    trucoParticular :: Truco
 } deriving Show
 
 pista = 1000
 
-deReversa :: Auto -> Auto
+type Nombre = String
+type Truco = Auto -> Auto 
+type NivelNafta = Float
+type Velocidad = Float
+
+-- 1) Type Alias en Data y en las funciones (L)
+-- 2) Definir datas de auto con funciones de truco (L)
+-- 3) Delegar esVocal en cuantasVocales (L)
+-- 4) incrementarVelocidad sacar ultima guarda (L)
+-- 5) definir funcion "acelerar" o "sumarVelocidad" (L)
+-- 6) Delegar turbo en otras funciones, aprovechar la funcion de la correccion 5) y componer. (L)
+
+deReversa :: Truco
 deReversa unAuto = unAuto {nivelNafta = (+pista/5).nivelNafta $ unAuto}
 
-impresionar :: Auto -> Auto
+impresionar :: Truco 
 impresionar unAuto = unAuto {velocidad = (*2).velocidad $ unAuto}
 
-nitro :: Auto -> Auto
-nitro unAuto = unAuto {velocidad = (+15).velocidad $ unAuto}
+sumarVelocidad :: Auto -> Velocidad -> Auto
+sumarVelocidad unAuto numeroX = unAuto {velocidad = (+numeroX).velocidad $ unAuto}
+nitro :: Truco
+nitro unAuto = sumarVelocidad unAuto 15
 
-fingirAmor :: Auto -> String  -> Auto 
-fingirAmor unAuto otroAuto = unAuto {nombreEnamorade = otroAuto}
+fingirAmor :: Nombre -> Truco 
+fingirAmor otroAuto unAuto  = unAuto {nombreEnamorade = otroAuto}
 
 rochaMcQueen = Auto {
     nombre = "RochaMcQueen",
     nivelNafta = 300,
     velocidad = 0,
     nombreEnamorade = "Ronco",
-    trucoParticular = "deReversa"
+    trucoParticular = deReversa
 }
 
 biankerr = Auto {
@@ -33,7 +49,7 @@ biankerr = Auto {
     nivelNafta = 500,
     velocidad = 20,
     nombreEnamorade = "Tinch",
-    trucoParticular = "impresionar"
+    trucoParticular = impresionar
 }
 
 gushtav = Auto {
@@ -41,7 +57,7 @@ gushtav = Auto {
     nivelNafta = 200,
     velocidad = 130,
     nombreEnamorade = "PetiLaLinda",
-    trucoParticular = "nitro"
+    trucoParticular = nitro
 }
 
 rodra = Auto {
@@ -49,50 +65,39 @@ rodra = Auto {
     nivelNafta = 0,
     velocidad = 50,
     nombreEnamorade = "Taisa",
-    trucoParticular = "fingirAmor"
+    trucoParticular = (fingirAmor "Petra")
 }
 
 -- Punto 2
-cuantasVocales :: Auto -> Int 
-cuantasVocales (Auto _ _ _ nombreEnamorade _) = length.filter(`elem` "aeiou") $ nombreEnamorade
+esVocal :: Auto -> Nombre
+esVocal (Auto _ _ _ nombreEnamorade _) = filter(`elem`  "aeiouAEIOU") $  nombreEnamorade
 
-incrementarVelocidad :: Auto -> Auto
+cuantasVocales :: Auto -> Int 
+cuantasVocales = length.esVocal 
+
+incrementarVelocidad :: Truco
 incrementarVelocidad unAuto
-   | (<=2) (cuantasVocales unAuto) = unAuto {velocidad = (+15).velocidad $ unAuto}
-   | (<=4) (cuantasVocales unAuto) = unAuto {velocidad = (+20).velocidad $ unAuto}
-   | (>4) (cuantasVocales unAuto) = unAuto {velocidad = (+30).velocidad $ unAuto}
-   | otherwise = unAuto {velocidad = (+0).velocidad $ unAuto}
+   | (<=2) (cuantasVocales unAuto) = sumarVelocidad unAuto 15
+   | (<=4) (cuantasVocales unAuto) = sumarVelocidad unAuto 20
+   | otherwise = sumarVelocidad unAuto 30
 
 -- Punto 3
 puedeRealizarTruco :: Auto -> Bool
 puedeRealizarTruco (Auto _ nivelNafta velocidad _ _) = (>0)nivelNafta && (<100)velocidad 
 
 -- Punto 4
-comboLoco :: Auto -> Auto
+comboLoco :: Truco
 comboLoco = nitro.deReversa
 
-queTrucazo :: Auto -> String -> Auto 
-queTrucazo unAuto nombre = incrementarVelocidad $ (fingirAmor unAuto nombre)
+queTrucazo :: Auto -> Nombre -> Auto 
+queTrucazo unAuto nombre = incrementarVelocidad $ (fingirAmor nombre unAuto)
 
-turbo :: Auto -> Auto 
-turbo unAuto = unAuto {velocidad =  (+)((*10).nivelNafta $ unAuto)(velocidad $ unAuto), nivelNafta = 0}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+turbo :: Truco 
+turbo = naftaCero.nuevaVelocidad
+nuevaVelocidad :: Auto->Auto
+nuevaVelocidad unAuto = sumarVelocidad unAuto (naftaPorDiez  unAuto)  
+naftaPorDiez :: Auto -> NivelNafta   
+naftaPorDiez unAuto =  (*10).nivelNafta $ unAuto
+naftaCero :: Truco 
+naftaCero unAuto = unAuto {nivelNafta=0}
 
